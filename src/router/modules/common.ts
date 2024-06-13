098f7e04fdfde14309c975a606c4dc75e39ce445
wanggaoxian <mlabor@163.com>
2024-06-13 09:10:04 +0800

    路由修改

--- src/router/modules/common.ts
+++ src/router/modules/common.ts
@@ -2,13 +2,13 @@ import {RouteRecordRaw} from "vue-router";
 
 export const Common: RouteRecordRaw[] = [
   {
-    path: '/login',
+    path: '/',
     component: () => import('@/views/common/Login.vue'),
     meta: {title: '登录', hideTabs: true}
   },
-  {
-    // 找不到路由重定向到404页面
-    path: "/:pathMatch(.*)",
-    component: () => import('@/views/common/404.vue'),
-  },
+  // {
+  //   // 找不到路由重定向到404页面
+  //   path: "/:pathMatch(.*)",
+  //   component: () => import('@/views/common/404.vue'),
+  // },
 ]
